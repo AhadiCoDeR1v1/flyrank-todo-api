4e6fed7d78a9ccb7b2af5437b48c6a2a815b38e8
Ahad iqbal <iahad2706@gmail.com>
2026-07-16 16:12:03 +0500

    Stage 5: Swagger UI

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');   // Import UI rendering library
+const swaggerDocument = require('./openapi.json');
 const app = express();
 const PORT = 3000;
 
 
 app.use(express.json());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // In-Memory mock data list
 let tasks = [
